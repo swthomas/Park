0b51ae054576f68bb9521d87024e5b7a24327aad
Seth Thomas <swthomas34@gmail.com>
2017-07-29 07:35:21 -0600

    angular

--- ParkREST/WebContent/ng/app/static/home/home.component.js
+++ ParkREST/WebContent/ng/app/static/home/home.component.js
@@ -1,7 +1,13 @@
 angular.module('static').component('home', {
 	templateUrl : 'ng/app/static/home/home.component.html',
-	controller : function(staticService, $scope, $location) {
+	controller : function(staticService, $scope, $location, NgMap) {
 		var vm = this;
+		  
+		NgMap.getMap().then(function(map) {
+			    console.log('map', map);
+			    vm.map = map;
+			  });
+		  
 		vm.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-eCSz4m2r6WczpOcJANrtbF8xps8EDuU&libraries=places";
 		
 		vm.parkingSpots = [];
@@ -10,20 +16,17 @@ angular.module('static').component('home', {
 		vm.selectedParkingSpot = null;
 		vm.showMap = true;
 		
-
-		
 		
 //		Get array of parkingSpots and address info
 		staticService.listParkingSpots().then(function(res){
-			
 			console.log(res.data);
 			
-		vm.parkingSpots = res.data;
-		for (var i = 0; i < vm.parkingSpots.length; i++) {
-			vm.markers[i] = '[' + vm.parkingSpots[i].parkingSpotAddress.latitude + ',' + 
-				vm.parkingSpots[i].parkingSpotAddress.longitude + ','+ JSON.stringify(vm.parkingSpots[i]) + ']';
-		}
-		return vm.markers;
+			vm.parkingSpots = res.data;
+			for (var i = 0; i < vm.parkingSpots.length; i++) {
+				vm.markers[i] = '[' + vm.parkingSpots[i].parkingSpotAddress.latitude + ',' + 
+					vm.parkingSpots[i].parkingSpotAddress.longitude + ','+ JSON.stringify(vm.parkingSpots[i]) + ']';
+			}
+			return vm.markers;
 		})
 		
 		
@@ -33,14 +36,13 @@ angular.module('static').component('home', {
 		vm.showParkingSpot = function(mk, data) {
 			var modifiedStrArray = data.split(",").slice(2).join(",").split("");
 			modifiedStrArray.pop();
-			modifiedStrArray = modifiedStrArray.join("")
+			modifiedStrArray = modifiedStrArray.join("");
+			
 			vm.selectedParkingSpot = JSON.parse(modifiedStrArray);
+		    vm.map.showInfoWindow('foo-iw', this);
 		}
 
-		
-		vm.goToStore = function(s) {
-			$location.path('store/store/'+ s.id);
-		}
+
       
 	 },
 	 controllerAs: 'vm'
--- prk.sql
+++ prk.sql
@@ -380,7 +380,7 @@ START TRANSACTION;
 USE `prk`;
 INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (1, '32 S Tejon St.', NULL, 80903, 'Colorado Springs', 'CO', 38.832697, -104.824003);
 INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (2, '20 E Colorado Ave', NULL, 80903, 'Colorado Springs', 'CO', 38.832504, -104.824278);
-INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (3, '17 S Tejon St', NULL, 80903, 'Colorado Springs', 'CO', 38.833065, -104.823034);
+INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (3, '17 S Tejon St', NULL, 80903, 'Colorado Springs', 'CO', 38.833065, 104.823034);
 INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (4, '21 S Tejon St', NULL, 80903, 'Colorado Springs', 'CO', 38.832959, -104.823043);
 INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (5, '1 S Nevada Ave', NULL, 80903, 'Colorado Springs', 'CO', 38.832799, -104.821202);
 INSERT INTO `parkingSpotAddress` (`id`, `street`, `street2`, `postalCode`, `city`, `state`, `latitude`, `longitude`) VALUES (6, '18 S Nevada Ave', NULL, 80903, 'Colorado Springs', 'CO', 38.833053, -104.822296);
@@ -403,12 +403,12 @@ INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpot
 INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (3, 'Parking spot for your car at my house', 2.99, 1, 3);
 INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (4, 'Parking spot for your car at my house', 2.99, 1, 4);
 INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (5, 'Parking spot for your car at my house', 2.99, 1, 5);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (6, 'Parking spot for your car at my house', 4, 1, 6);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (7, 'Parking spot for your car at my house', 4, 1, 7);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (8, 'Parking spot for your car at my house', 5, 1, 8);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (9, 'Parking spot for your car at my house', 10, 1, 9);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (10, 'Parking spot for your car at my house', 10, 1, 10);
-INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (11, 'Parking spot for your car at my house', 11, 1, 11);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (6, 'Parking spot for your car at my house', 4.25, 1, 6);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (7, 'Parking spot for your car at my house', 4.25, 1, 7);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (8, 'Parking spot for your car at my house', 5.25, 1, 8);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (9, 'Parking spot for your car at my house', 10.25, 1, 9);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (10, 'Parking spot for your car at my house', 10.25, 1, 10);
+INSERT INTO `parkingSpot` (`id`, `description`, `rate`, `listerId`, `parkingSpotAddressId`) VALUES (11, 'Parking spot for your car at my house', 11.25, 1, 11);
 
 COMMIT;
 
@@ -452,8 +452,7 @@ INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `
 INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (2, '2017-07-01 03:00:00', '2017-07-01 03:15:00', 2.99, 1, 1, 1);
 INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (3, '2017-01-01 12:00:00', '2017-01-01 01:00:00', 2.99, 1, 1, 1);
 INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (4, '2017-01-01 12:00:00', '2017-01-01 12:45:00', 2.99, 1, 1, 1);
-INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (5, '2017-07-02 14:00:00', '2017-07-02 20:16:00', 2.99, 1, 1, 2);
-INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (6, '2017-07-02 14:00:00', '2017-09-02 14:00:00', 2.99, 1, 1, 3);
+INSERT INTO `reservation` (`id`, `reservedFromDate`, `reservedToDate`, `rate`, `userId`, `vehicleId`, `parkingSpotId`) VALUES (5, '2017-07-02 14:00:00', '2017-07-02 14:16:00', 2.99, 1, 1, 2);
 
 COMMIT;
 
