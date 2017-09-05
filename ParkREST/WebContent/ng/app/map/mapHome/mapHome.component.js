e9a8da2130251e65055989766817737a9f7691cf
Developer <stephen.e.chiang@gmail.com>
2017-09-05 15:14:51 +0200

    added angular cookie law

--- ParkREST/WebContent/ng/app/map/mapHome/mapHome.component.js
+++ ParkREST/WebContent/ng/app/map/mapHome/mapHome.component.js
@@ -1,44 +1,48 @@
 angular.module('map').component('mapHome', {
-	templateUrl : 'ng/app/map/mapHome/mapHome.component.html',
-	controller : function(mapService, $location, NgMap) {
+					templateUrl : 'ng/app/map/mapHome/mapHome.component.html',
+					controller : function(mapService, $location, NgMap) {
 
-		var vm = this;
-		vm.parkingSpots = [];
-		vm.markers = [];
-		vm.selectedParkingSpot = null;
-		vm.showMap = true;
-		vm.mapCenter = "Colorado Springs, CO";
-		  
-		NgMap.getMap().then(function(map) {
-			console.log('map', map);
-			vm.map = map;
-		});		
+						var vm = this;
+						vm.parkingSpots = [];
+						vm.markers = [];
+						vm.selectedParkingSpot = null;
+						vm.showMap = true;
+						vm.mapCenter = "Colorado Springs, CO";
 
-		
-//		Get array of parkingSpots and address info
-		mapService.listParkingSpots().then(function(res){
-			
-			vm.parkingSpots = res.data;
-			for (var i = 0; i < vm.parkingSpots.length; i++) {
-				vm.markers[i] = '[' + vm.parkingSpots[i].parkingSpotAddress.latitude + ',' + 
-					vm.parkingSpots[i].parkingSpotAddress.longitude + ','+ JSON.stringify(vm.parkingSpots[i]) + ']';
-			}
-			return vm.markers;
-		});
-				
-		// first argument is metadata from the map 
-		// second argument it marker data provided by 'staticService.listParkingSpots()' above
-		vm.showParkingSpot = function(mk, data) {
-			var modifiedStrArray = data.split(",").slice(2).join(",").split("");
-			modifiedStrArray.pop();
-			modifiedStrArray = modifiedStrArray.join("");
-			
-			vm.selectedParkingSpot = JSON.parse(modifiedStrArray);
-		    vm.map.showInfoWindow('foo-iw', this);
-		};
+						NgMap.getMap().then(function(map) {
+							vm.map = map;
+						});
 
+						// Get array of parkingSpots and address info
+						mapService.listParkingSpots().then(function(res) {
+							vm.parkingSpots = res.data;
+							for (var i = 0; i < vm.parkingSpots.length; i++) {
+								vm.markers[i] = '['
+										+ vm.parkingSpots[i].parkingSpotAddress.latitude
+										+ ','
+										+ vm.parkingSpots[i].parkingSpotAddress.longitude
+										+ ','
+										+ JSON
+												.stringify(vm.parkingSpots[i])
+										+ ']';
+							}
+							return vm.markers;
+						});
 
-      
-	 },
-	 controllerAs: 'vm'
-})
+						// first argument is metadata from the map
+						// second argument it marker data provided by
+						// 'staticService.listParkingSpots()' above
+						vm.showParkingSpot = function(mk, data) {
+							var modifiedStrArray = data.split(",").slice(2)
+									.join(",").split("");
+							modifiedStrArray.pop();
+							modifiedStrArray = modifiedStrArray.join("");
+
+							vm.selectedParkingSpot = JSON
+									.parse(modifiedStrArray);
+							vm.map.showInfoWindow('foo-iw', this);
+						};					
+
+					},
+					controllerAs : 'vm'
+				})
